1e7c72d520d039db50f6a127f7a9485572b5accd
Shivampg1 <147384496+Shivampg1@users.noreply.github.com>
2025-12-03 10:55:21 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./i18n";
+import "./i18n/i18n";
 import "./index.css";
 
 import { registerSW } from "virtual:pwa-register";  // ⬅️ add this
