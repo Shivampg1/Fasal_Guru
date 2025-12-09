9b7215b7e4b3e31c89121d78acb5be1201c54f1a
Shivampg1 <147384496+Shivampg1@users.noreply.github.com>
2025-12-09 17:07:44 +0530

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -56,11 +56,10 @@ const Navigation = ({ activeModule, onModuleChange }: NavigationProps) => {
   },
 
   { 
-    id: "weather",
-    label: "Weather",
-    icon: <Cloud className="h-5 w-5" />,
-    color: "secondary",
-    link: "https://weather-app-navy-nine-35.vercel.app/"
+    id: "voice-assistant",
+    label: "Voice Assistant", 
+    icon: <Mic className="h-5 w-5" />, 
+    color: "indigo"
   },
 ];
 
