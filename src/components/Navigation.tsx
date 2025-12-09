3208dd6860d060f1e3ac94419597eb1893ae514b
Shivampg1 <147384496+Shivampg1@users.noreply.github.com>
2025-12-09 17:02:08 +0530

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import VoiceAssistantModule from "./VoiceAssistantModule";
+
 import { 
   Home, 
   Camera, 
